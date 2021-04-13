29382d43e6f296612dd83433e96c79340648a659
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2021-04-13 11:54:36 +0200

    Better comments for functions in ShadowMemory.cpp

--- ShadowMemory.cpp
+++ ShadowMemory.cpp
@@ -6,11 +6,17 @@ using std::vector;
 
 static vector<bool> dirtyPages;
 static unsigned long SHADOW_ALLOCATION;
+
+// This shadow memory keeps 1 bit for each byte of application memory,
+// telling if that byte has been initialized by a write access
 static std::vector<uint8_t*> shadow;
+
 // This second shadow memory keeps 1 additional bit for each byte of application memory,
 // telling if that byte has been read by a read access considered uninitialized
 static std::vector<uint8_t*> readShadow;
 
+// Returns a pair containing the index to be used to retrieve the correct shadow page
+// and an offset required to get the correct address inside that page
 static std::pair<unsigned, unsigned> getShadowAddrIdxOffset(ADDRINT addr){
     unsigned retOffset = (unsigned) (threadInfos[0] - addr);
     unsigned offset = retOffset;
@@ -19,6 +25,8 @@ static std::pair<unsigned, unsigned> getShadowAddrIdxOffset(ADDRINT addr){
     return std::pair<unsigned, unsigned>(shadowIdx, retOffset);
 }
 
+// Given the index and the offset returned from the previous function,
+// return the corresponding shadow memory address
 static uint8_t* getShadowAddrFromIdx(unsigned shadowIdx, unsigned offset){
     bool needsCeiling = offset % 8 != 0;
     
@@ -49,6 +57,8 @@ uint8_t* getShadowAddr(ADDRINT addr){
     return ret;
 }
 
+// Given a shadow address, return the corresponding shadow address of the read shadow memory
+// (the one containing information about bytes read by uninitialized read accesses)
 static uint8_t* getReadShadowFromShadow(uint8_t* shadowAddr, unsigned shadowIdx){
     unsigned offset = shadowAddr - shadow[shadowIdx];
 
@@ -64,6 +74,9 @@ static uint8_t* getReadShadowFromShadow(uint8_t* shadowAddr, unsigned shadowIdx)
     return readShadow[shadowIdx] + offset;
 }
 
+// Function invoked whenever a write access is executed.
+// It sets the corresponding shadow memory area to 1 and resets the corresponding
+// read shadow memory area to 0
 void set_as_initialized(ADDRINT addr, UINT32 size){
     std::pair<unsigned, unsigned> idxOffset = getShadowAddrIdxOffset(addr);
     unsigned shadowIdx = idxOffset.first;
@@ -113,6 +126,8 @@ void set_as_initialized(ADDRINT addr, UINT32 size){
     }
 }
 
+// Function invoked whenever an uninitialized read is executed.
+// It sets the corresponding read shadow memory area to 1.
 static void set_as_read_by_uninitialized_read(unsigned size, uint8_t* shadowAddr, unsigned offset, unsigned shadowIdx){
     uint8_t* readShadowAddr = getReadShadowFromShadow(shadowAddr, shadowIdx);
 
@@ -141,6 +156,22 @@ static void set_as_read_by_uninitialized_read(unsigned size, uint8_t* shadowAddr
     }
 
 }
+
+// Function to retrieve the bytes of the given address and size which are considered not initialized.
+// The returned interval contains the offset of the bytes from the beginning of the access.
+// For instance, if a write already initialized bytes [0x0, 0xa] of memory and the given address and size access
+// [0x0, 0xf], the returned pair would be (11, 15).
+
+// NOTE: this is not a precise result. If the access represented by |addr| and |size| is fully initialized except 1 byte,
+// the returned pair will report as uninitialized the whole interval from the uninitialized byte to the end of the
+// access.
+// This has been done because:
+//  [*] It shouldn't be very frequent that an access to an area of the stack has an uninitialized "hole"
+//      (this may happen, though, for instance with functions requiring a particular address alignment)
+//  [*] It would have been quite expensive to return the precise interval as it would require to always scan all 
+//      the bytes of the access and, in case the "hole" was in the middle
+//      of the access, it may have required to return a set of intervals, thus making the following steps
+//      of the tool more complex.
 
 std::pair<int, int> getUninitializedInterval(ADDRINT addr, UINT32 size){
     std::pair<unsigned, unsigned> idxOffset = getShadowAddrIdxOffset(addr);
@@ -206,6 +237,8 @@ std::pair<int, int> getUninitializedInterval(ADDRINT addr, UINT32 size){
         return std::pair<int, int>(-1, -1);
 }
 
+// Returns true if the access represented by the given address and size (at least one of its bytes, actually)
+// is read by an uninitialized read access
 bool isReadByUninitializedRead(ADDRINT addr, UINT32 size){
     std::pair<unsigned, unsigned> idxOffset = getShadowAddrIdxOffset(addr);
     unsigned shadowIdx = idxOffset.first;
@@ -250,6 +283,8 @@ bool isReadByUninitializedRead(ADDRINT addr, UINT32 size){
     return isRead;
 }
 
+// Resets all the shadow memory addresses above or equal to |addr| to 0.
+// This is invoked on return instructions, when a stack frame is "freed".
 void reset(ADDRINT addr){
     std::pair<unsigned, unsigned> idxOffset = getShadowAddrIdxOffset(addr);
     unsigned shadowIdx = idxOffset.first;
@@ -266,6 +301,7 @@ void reset(ADDRINT addr){
     }
 }
 
+// Shadow memory initialization
 void shadowInit(){
     shadow.reserve(25);
     readShadow.reserve(25);
@@ -288,6 +324,9 @@ void shadowInit(){
         }
         readShadow.push_back(newMap);
     }
+    // The address contained by the stack pointer at the beginning of the application entry point is initialized
+    // outside the function itself (i.e. by the loader) but the entry point reads it through a pop instruction.
+    // In order to avoid to report a false positive, set as initialized the shadow memory mirroring that address.
     *shadow[0] = 0xff;
     dirtyPages[0] = true;
 }
