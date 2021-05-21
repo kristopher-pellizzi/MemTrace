64cb72bd7e74bcef6a096a33eac69ffe8c6949f0
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2021-05-21 12:54:03 +0200

    Reduced reported reads due to free + malloc
    In order to avoid that malloc reads bytes considered uninitialized, the
    first 16 bytes of a chunk's payload on an x86_64 architecture using
    glibc implementation are not re-initialized during a call to free.
    This is because free itself writes there some pointers that a following
    malloc may read.
    Note however that some not relevant uninitialized reads may still be
    reported, in some cases.
    More infos in issue #2.

--- x86_64_linux_malloc_handlers.h
+++ x86_64_linux_malloc_handlers.h
@@ -28,9 +28,12 @@ ADDRINT malloc_get_block_beginning(ADDRINT addr){
 
 // This function returns a set of pairs representing the portions of an allocated block to be reinitialized on free.
 // This is required because in some cases it may lead to false positives reinitializing the whole block
-// (e.g. malloc->free->malloc->free, where the second malloc reuses the first freed one)
+// (e.g. malloc->free->malloc->free, where the second malloc reuses the first freed one).
+// On x86_64 architectures, using the glibc implementation of malloc and free, we should also avoid re-initializing
+// the first 16 bytes of the payload, as they are written by a call to free in order to keep metadata
+// about the freed chunk and may be used by successive calls to malloc.
 std::set<std::pair<ADDRINT, size_t>> malloc_mem_to_reinit(ADDRINT block_addr, size_t blockSize){
     std::set<std::pair<ADDRINT, size_t>> ret;
-    ret.insert(std::pair<ADDRINT, size_t>(block_addr + 16, blockSize - 16));
+    ret.insert(std::pair<ADDRINT, size_t>(block_addr + 32, blockSize - 32));
     return ret;
 }
