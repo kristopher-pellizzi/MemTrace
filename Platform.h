a2b27e78f48d51be5d691a660bd8a62f18d8cc04
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2021-05-30 18:45:09 +0200

    Updated architecture detection macros
    Added MSVC (MicroSoft VisualStudio Compiler) compatible macros to detect
    the underlying architecture (x86 vs x86_64)

--- Platform.h
+++ Platform.h
@@ -9,15 +9,16 @@
 static const unsigned long pagesize = sysconf(_SC_PAGESIZE);
 
 // NOTE: this has been tested to work with gcc/g++ compiler. It could not work if another compiler is
-// used (e.g. MSVC++, Clang...). If you are using a different compiler, be sure to extend this header
+// used (e.g. MSVC++, Clang...). If you are using a different compiler, be sure to check and 
+// extend this header if needed.
 
 // Architecture detection. Note that Intel PIN only supports X86 and X86_64 architectures. It is useless 
 // to try and identify unsupported architectures.
-#if defined(__amd64__)
+#if defined(__amd64__) || defined(_M_AMD64) || defined(_M_X64)
     #define X86_64
     #define MMAP_NUM 9
     #define STACK_SHADOW_INIT 0xff
-#elif defined(__i386__)
+#elif defined(__i386__) || defined(_M_IX86)
     #define X86
     #define MMAP_NUM 90
     #define STACK_SHADOW_INIT 0xf0
@@ -29,7 +30,8 @@ static const unsigned long pagesize = sysconf(_SC_PAGESIZE);
 
 
 // OS detection. The list is not complete. The tool has been developed and tested on a Linux platform.
-// If needed, extend this to detect your underlying OS
+// If needed, extend this to detect your underlying OS.
+// Note that it must support Intel PIN to use this tool.
 #if defined(__linux__)
     #define LINUX
 #elif defined(_WIN32)
