7e6b7d74bff1f8cb20caccc7178844fd4532e9f2
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2021-07-23 10:30:08 +0200

    Fixed paths arguments

--- util/gdb_caller.py
+++ util/gdb_caller.py
@@ -5,11 +5,11 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("executable_rel_path",
-        help = "Relative path of the executable to be run"
+    parser.add_argument("executable_path",
+        help = "Path of the executable to be run"
     )
-    parser.add_argument("testcase_rel_path",
-        help = "Relative path of the directory containing both the input file and the arguments file"
+    parser.add_argument("testcase_path",
+        help = "Path of the directory containing both the input file and the arguments file"
     )
 
     return parser.parse_args()
@@ -19,9 +19,8 @@ def main():
     args = parse_args()
     
     # base_path is the full path of the current working directory as a bytes string
-    base_path = os.fsencode(os.path.realpath(os.getcwd()))
-    testcase_path = os.path.join(base_path, os.fsencode(args.testcase_rel_path))
-    executable_path = os.path.join(base_path, os.fsencode(args.executable_rel_path))
+    testcase_path = os.fsencode(args.testcase_path)
+    executable_path = os.fsencode(args.executable_path)
     args_path = os.path.join(testcase_path, b"argv")
 
     argv = [b'gdb', b'--args', executable_path]
