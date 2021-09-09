5359b003583f52768176cf6b45b7025bf33d0d44
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2021-09-09 15:02:44 +0200

    Implemented function |findLibByName|

--- python_modules/libFinder.py
+++ python_modules/libFinder.py
@@ -5,3 +5,10 @@ def findLib(ip_str: str, load_bases: List[Tuple[str, str]]) -> Tuple[str, str]:
     ip = int(ip_str, 16)
     name, addr = max(filter(lambda x: x[1] <= ip, map(lambda x: (x[0], int(x[1], 16)), load_bases)), key = lambda x: x[1])
     return (name, hex(addr))
+
+
+def findLibByName(name: str, load_bases: List[Tuple[str, str]]) -> str:
+    '''Returns the base address of the library whose name is passed as |name|'''
+    for lib_name, lib_addr in load_bases:
+        if lib_name == name:
+            return lib_addr
