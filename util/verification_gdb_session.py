8aab19d0563362e3efa680c2f18befaac53d2f44
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2021-09-21 17:55:04 +0200

    Implement cmds to automagically convert addresses
    Implemented commands overlap-stack-addr and overlap-heap-addr to convert
    a stack/heap address reported in the overlaps report into a valid
    address for the program being debugged in gdb

--- util/verification_gdb_session.py
+++ util/verification_gdb_session.py
@@ -1,6 +1,8 @@
 import gdb
 import os
 import sys
+from functools import reduce
+from time import sleep
 
 def import_modules(script_dir):
     modules_path = os.path.realpath(os.path.join(script_dir, "..", "python_modules"))
@@ -14,7 +16,7 @@ sys.path.append(path)
 import_modules(path)
 
 from binOverlapParser import parse
-from libFinder import findLib
+from libFinder import findLib, findLibByName
 
 gdb.execute("unset env LINES")
 gdb.execute("unset env COLUMNS")
@@ -27,6 +29,7 @@ testcase_path = os.path.realpath(s.split("=")[1].strip()[1:-1])
 overlaps_path = os.path.join(testcase_path, "overlaps.bin")
 parsed_ret = parse(bin_report_dir = testcase_path)
 load_bases = parsed_ret.load_bases
+stack_base = parsed_ret.stack_base
 
 def breakpoint_created(ev):
     def is_watchpoint():
@@ -56,6 +59,21 @@ def inferior_is_running():
     return gdb.selected_inferior().pid != 0
 
 
+def on_start(ev):
+    # Whenever we hit the breakpoint set to "_start"
+    if isinstance(ev, gdb.BreakpointEvent):
+        stack_cmd.set_stack_addr()
+        gdb.events.stop.disconnect(on_start)
+        gdb.execute("r")
+
+
+def try_get_heap_base(ev):
+    if isinstance(ev, gdb.BreakpointEvent):
+        if heap_cmd.set_heap_addr():
+            gdb.events.stop.disconnect(try_get_heap_base)
+
+
+
 class BreakOverlapCommand(gdb.Command):
     def __init__(self):
         super(BreakOverlapCommand, self).__init__("break-overlap", gdb.COMMAND_USER)
@@ -78,13 +96,96 @@ class BreakOverlapCommand(gdb.Command):
         gdb.execute("x/i {0}".format(addr))
 
 
+class OverlapStackCommand(gdb.Command):
+    def __init__(self):
+        super(OverlapStackCommand, self).__init__("overlap-stack-base", gdb.COMMAND_USER)
+        self.stack_addr = None
+
+    def invoke(self, arg, from_tty):
+        print(hex(self.stack_addr))
+
+    def set_stack_addr(self):
+        s = gdb.execute("print/x ${0}".format(sp), to_string = True)
+        addr = int(s.split('=')[1].strip(), 16)
+        self.stack_addr = addr
+
+
+class OverlapHeapCommand(gdb.Command):
+    def __init__(self):
+        super(OverlapHeapCommand, self).__init__("overlap-heap-base", gdb.COMMAND_USER)
+        self.heap_addr = None
+
+    def invoke(self, arg, from_tty):
+        if self.heap_addr is None:
+            print("Heap is not initialized yet")
+        else:
+            print(hex(self.heap_addr))
+
+    def set_heap_addr(self):
+        def get_sbrk_base():
+            nonlocal s
+            left_brace_idx = s.find("{")
+            right_brace_idx = s.find("}")
+            s = s[left_brace_idx + 1 : right_brace_idx]
+            splitted = map(lambda x: x.split('='), s.split(','))
+            d = dict()
+            for key, val in splitted:
+                d[key.strip()] = val.strip().split()[0].strip()
+            return int(d['sbrk_base'], 16)
+
+        s: str = gdb.execute("print mp_", to_string = True)
+        sbrk_base = get_sbrk_base()
+        if sbrk_base != 0:
+            self.heap_addr = sbrk_base
+            return True
+        else:
+            return False
+
+
+class OverlapHeapAddrCommand(gdb.Command):
+    def __init__(self):
+        super(OverlapHeapAddrCommand, self).__init__("overlap-heap-addr", gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        if not arg:
+            print("Command requires an address as parameter")
+            return
+        
+        if heap_cmd.heap_addr is None:
+            print("Heap base address not found yet")
+            print("Set a breakpoint somewhere the heap is already initialized and hit it.")
+            return
+
+        try:
+            addr = int(arg.strip(), 0)
+            pin_heap_base = int(findLibByName("Heap", load_bases), 16)
+            print(hex(addr - pin_heap_base + heap_cmd.heap_addr))
+        except ValueError:
+            print("Not valid address")
+
+
+class OverlapStackAddrCommand(gdb.Command):
+    def __init__(self):
+        super(OverlapStackAddrCommand, self).__init__("overlap-stack-addr", gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        if not arg:
+            print("Command requires an address as parameter")
+            return
+
+        try:
+            addr = int(arg.strip(), 0)
+            pin_stack_base = int(stack_base, 16)
+            print(hex(addr - pin_stack_base + stack_cmd.stack_addr))
+        except ValueError:
+            print("Not valid address")
+
+
 class VmmapCommand(gdb.Command):
     def __init__(self):
         super(VmmapCommand, self).__init__("memmap", gdb.COMMAND_USER)
 
     def invoke(self, arg: str, from_tty):
-        # TODO: now add arg, and return only the requested addresses in case it is a string with the library name or
-        # a string with an address
         if not inferior_is_running():
             print("No running process found")
             return
@@ -147,12 +248,31 @@ class VmmapCommand(gdb.Command):
                 printed_line = True
         print("\u001b[0m")    
 
-
+# Define new commands
 BreakOverlapCommand()
-gdb.execute("alias bro=break-overlap")
-gdb.execute("r")
-
+gdb.execute("alias bro=break-overlap", to_string = True)
 VmmapCommand()
+stack_cmd = OverlapStackCommand()
+OverlapStackAddrCommand()
+gdb.execute("alias saddr=overlap-stack-addr")
+heap_cmd = OverlapHeapCommand()
+OverlapHeapAddrCommand()
+gdb.execute("alias haddr=overlap-heap-addr")
 
-s = gdb.execute("memmap /usr/lib/x86_64-linux-gnu/libc-2.31.so", to_string = True)
-gdb_base = int(s.split("\n")[0].strip().split()[1].strip(), 0)
+# Setup required information
+gdb.execute("r", to_string = True)
+s: str = gdb.execute("info registers", to_string = True)
+regs = set(map(lambda x: x.split()[0], s.splitlines()))
+arch = None
+sp = None
+if "rsp" in regs:
+    arch = 64
+    sp = "rsp"
+elif "esp" in regs:
+    arch = 32
+    sp = "esp"
+
+# Setup stack and heap base addresses
+gdb.events.stop.connect(on_start)
+gdb.events.stop.connect(try_get_heap_base)
+gdb.execute("r")
