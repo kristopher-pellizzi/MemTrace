0be4c079568c73a5aa2c09e562aba30ca19928a4
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2021-09-30 23:08:54 +0200

    Modified template to adapt to mem and reg emulator
    The emulator template is now modified by the python script in order to
    define the header of a memory or register instruction emulator according
    to the kind of the emulator

--- src/EmulatorTemplate.h
+++ src/EmulatorTemplate.h
@@ -1,10 +1,11 @@
-#include "../InstructionEmulator.h"
+#include "../../{2}InstructionEmulator.h"
+#include "../../misc/SrcRegsStatus.h"
 
 #ifndef {1}
 #define {1}
 
-class {0} : public InstructionEmulator{{
-    RETTYPE operator() ARGS override;
+class {0} : public {2}InstructionEmulator{{
+    void operator() {3} override;
 }};
 
 #endif //{1}
