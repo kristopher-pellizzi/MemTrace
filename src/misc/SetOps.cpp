f89fbe7eed8cbdb680a428f781a19a6b1f0e9f23
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2021-10-29 20:08:28 +0200

    Changed set<REG> to list<REG> + fix
    Previous commit (982360ac38cf3c9f0d48d789d607168da105c9d5) explains why
    we changed sets to lists.
    
    Fix:
    Since we use a list, there's no fast lookup function. So, we simply need
    to iterate over all the registers to perform the set difference.
    
    Of course, this makes the complexity raise up t O(N^2), but registers
    are always very few, thus making it a quite fast computation

--- src/misc/SetOps.cpp
+++ src/misc/SetOps.cpp
@@ -1,13 +1,16 @@
 #include "SetOps.h"
 
-set<REG>* setDiff(set<REG>* s, set<REG>* toRemove){
+list<REG>* setDiff(list<REG>* s, list<REG>* toRemove){
     if(s == NULL || toRemove == NULL)
         return s;
         
     for(auto iter = toRemove->begin(); iter != toRemove->end(); ++iter){
-        auto found = s->find(*iter);
-        if(found != s->end()){
-            s->erase(found);
+        for(auto sIter = s->begin(); sIter != s->end(); ++sIter){
+            if(*sIter == *iter){
+                auto toErase = sIter;
+                --sIter;
+                s->erase(toErase);
+            }
         }
     }
 
--- src/misc/SetOps.h
+++ src/misc/SetOps.h
@@ -1,10 +1,10 @@
-#include <set>
+#include <list>
 
 #include "pin.H"
 
 #ifndef SETOPS
 #define SETOPS
-using std::set;
+using std::list;
 
 /*
     Performs set difference operation |s| \ |toRemove|.
@@ -12,6 +12,6 @@ using std::set;
     @param s: Set to which the difference operator is applied
     @param toRemove: Set whose elements are removed from |s|, if present
 */
-set<REG>* setDiff(set<REG>* s, set<REG>* toRemove);
+list<REG>* setDiff(list<REG>* s, list<REG>* toRemove);
 
 #endif //SETOPS
