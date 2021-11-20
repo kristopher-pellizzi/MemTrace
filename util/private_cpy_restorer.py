7e27c2935cfc549c02080d7656bfc498b1d8669a
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2021-11-20 12:56:43 +0100

    Fixed declarations of variables
    Fixed declaration of |fuzzer_instance| by passing a parameter to
    |os.path.basename|
    
    Fixed declaration of |private_cpy_dir| by passing a bytes stream literal as a
    second parameter

--- util/private_cpy_restorer.py
+++ util/private_cpy_restorer.py
@@ -4,9 +4,9 @@ import shutil as su
 def restore_private_cpy_files(testcase_path):
     testcase_basename = os.path.basename(testcase_path)
     fuzzer_instance_path = os.path.dirname(testcase_path)
-    fuzzer_instance = os.path.basename()
+    fuzzer_instance = os.path.basename(fuzzer_instance_path)
     out_path = os.path.dirname(os.path.dirname(fuzzer_instance_path))
-    private_cpy_dir = os.path.join(out_path, 'private_cpy')
+    private_cpy_dir = os.path.join(out_path, b'private_cpy')
     testcase_private_cpy = os.path.join(private_cpy_dir, fuzzer_instance, testcase_basename)
 
     if os.path.exists(testcase_private_cpy):
