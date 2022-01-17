e5e7de0f56d87ac02939752492ccc5d6526ec767
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2022-01-17 20:16:13 +0100

    Fixed path of manifest file

--- Tests/install_manifest.sh
+++ Tests/install_manifest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IFS=$'\n'
-install_list=$(cat focal-server-cloudimg-amd64-root.manifest | sed 's/\t/ /g' | tr -s ' ' | sed '/snap:/d' | sed 's/ /=/g') 
+install_list=$(cat ../focal-server-cloudimg-amd64-root.manifest | sed 's/\t/ /g' | tr -s ' ' | sed '/snap:/d' | sed 's/ /=/g') 
 
 for line in $install_list 
 do
