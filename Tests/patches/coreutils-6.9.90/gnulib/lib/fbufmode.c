4dff96cb406995ee1f2d6be2785b99777939bdbc
Kris <kristopherfrancesco.pellizzi@mail.polimi.it>
2022-01-22 21:34:14 +0100

    Added patches for coreutils-6.9.90

---
+++ Tests/patches/coreutils-6.9.90/gnulib/lib/fbufmode.c
@@ -0,0 +1,86 @@
+/* Retrieve information about a FILE stream.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "fbufmode.h"
+
+#if HAVE___FLBF
+# include <stdio_ext.h>
+#endif
+
+int
+fbufmode (FILE *fp)
+{
+  /* Most systems provide FILE as a struct and the necessary bitmask in
+     <stdio.h>, because they need it for implementing getc() and putc() as
+     fast macros.  */
+#if defined _IO_ferror_unlocked     /* GNU libc, BeOS */
+# if HAVE___FLBF                    /* glibc >= 2.2 */
+  if (__flbf (fp))
+    return _IOLBF;
+# else
+  if (fp->_flags & _IO_LINE_BUF)
+    return _IOLBF;
+# endif
+  if (fp->_flags & _IO_UNBUFFERED)
+    return _IONBF;
+  return _IOFBF;
+#elif defined __sferror             /* FreeBSD, NetBSD, OpenBSD, MacOS X, Cygwin */
+  if (fp->_flags & __SLBF)
+    return _IOLBF;
+  if (fp->_flags & __SNBF)
+    return _IONBF;
+  return _IOFBF;
+#elif defined _IOERR                /* AIX, HP-UX, IRIX, OSF/1, Solaris, mingw */
+# if HAVE___FLBF                    /* Solaris >= 7 */
+  if (__flbf (fp))
+    return _IOLBF;
+# else
+  if (fp->_flag & _IOLBF)
+    return _IOLBF;
+# endif
+# if defined __sun && defined _LP64 /* Solaris/{SPARC,AMD64} 64-bit */
+#  define fp_ ((struct { unsigned char *_ptr; \
+			 unsigned char *_base; \
+			 unsigned char *_end; \
+			 long _cnt; \
+			 int _file; \
+			 unsigned int _flag; \
+		       } *) fp)
+  return fp_->_flag & (_IONBF | _IOFBF);
+# else
+  if (fp->_flag & _IONBF)
+    return _IONBF;
+  return _IOFBF;
+# endif
+#elif defined __UCLIBC__            /* uClibc */
+  if (fp->__modeflags & __FLAG_LBF)
+    return _IOLBF;
+  if (fp->__modeflags & __FLAG_NBF)
+    return _IONBF;
+  return _IOFBF;
+#elif defined __QNX__               /* QNX */
+  if (fp->_Mode & 0x400 /* _MLBF */)
+    return _IOLBF;
+  if (fp->_Mode & 0x800 /* _MNBF */)
+    return _IONBF;
+  return _IOFBF;
+#else
+ //#error "Please port gnulib fbufmode.c to your platform! Look at the setvbuf implementation."
+#endif
+}
---
+++ Tests/patches/coreutils-6.9.90/gnulib/lib/fwriting.c
@@ -0,0 +1,42 @@
+/* Retrieve information about a FILE stream.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "fwriting.h"
+
+bool
+fwriting (FILE *fp)
+{
+  /* Most systems provide FILE as a struct and the necessary bitmask in
+     <stdio.h>, because they need it for implementing getc() and putc() as
+     fast macros.  */
+#if defined _IO_ferror_unlocked     /* GNU libc, BeOS */
+  return (fp->_flags & (_IO_NO_READS | _IO_CURRENTLY_PUTTING)) != 0;
+#elif defined __sferror             /* FreeBSD, NetBSD, OpenBSD, MacOS X, Cygwin */
+  return (fp->_flags & __SWR) != 0;
+#elif defined _IOERR                /* AIX, HP-UX, IRIX, OSF/1, Solaris, mingw */
+  return (fp->_flag & _IOWRT) != 0;
+#elif defined __UCLIBC__            /* uClibc */
+  return (fp->__modeflags & __FLAG_WRITING) != 0;
+#elif defined __QNX__               /* QNX */
+  return ((fp->_Mode & 0x1 /* _MOPENR */) == 0
+	  || (fp->_Mode & 0x2000 /* _MWRITE */) != 0);
+#else
+ //#error "Please port gnulib fwriting.c to your platform!"
+#endif
+}
